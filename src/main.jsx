147a600537e8c3a972f0a3286751616ff00e4819
nekmichalakis <nekmichalakis@gmail.com>
2024-01-25 12:23:07 +0200

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './components/App'
 import './index.css'
@@ -25,10 +25,10 @@ i18next.init({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <I18nextProvider i18n={i18next}>
         <App />
       </I18nextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
